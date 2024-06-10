3d37c0dbbbdae6f7caa58833774f18744eeadaf4
ldearsley2 <ldearsley2@gmail.com>
2024-06-10 18:29:37 +0100

    CRUD

--- client/src/components/TodoItem.tsx
+++ client/src/components/TodoItem.tsx
@@ -2,13 +2,12 @@ import { Badge, Box, Flex, Spinner, Text } from "@chakra-ui/react";
 import { FaCheckCircle } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
 import { Todo } from "./TodoList";
-import { useMutation } from "@tanstack/react-query";
+import { queryOptions, useMutation, useQueryClient } from "@tanstack/react-query";
 import { BASE_URL } from "../App";
 
 const TodoItem = ({ todo }: { todo: Todo }) => {
 
-    console.log(todo);
-
+    const queryClient = useQueryClient()
     const {mutate:updateTodo, isPending:isUpdating} = useMutation({
         mutationKey:["updateTodo"],
         mutationFn: async() => {
@@ -25,8 +24,32 @@ const TodoItem = ({ todo }: { todo: Todo }) => {
             } catch (error) {
                 console.log(error)
             }
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({queryKey: ["todos"]})
         }
-    })
+    });
+
+    const {mutate: deleteTodo, isPending: isDeleting} = useMutation({
+        mutationKey: ["deleteTodo"],
+        mutationFn: async() => {
+            try {
+                const res = await fetch(BASE_URL + `/todos/${todo._id}`, {
+                    method: "DELETE",
+                });
+                const data = await res.json()
+                if(!res.ok) {
+                    throw new Error(data.error || "Something went wrong");
+                }
+                return data
+            } catch (error) {
+                console.log(error)
+            }
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({queryKey: ["todos"]})
+        }
+    });
 
 	return (
 		<Flex gap={2} alignItems={"center"}>
@@ -61,8 +84,9 @@ const TodoItem = ({ todo }: { todo: Todo }) => {
                     {!isUpdating && <FaCheckCircle size={20} />}
                     {isUpdating && <Spinner size={"sm"} />}
 				</Box>
-				<Box color={"red.500"} cursor={"pointer"}>
-					<MdDelete size={25} />
+				<Box color={"red.500"} cursor={"pointer"} onClick={() => deleteTodo()}>
+                    {!isDeleting && <MdDelete size={25} />}
+                    {isDeleting && <Spinner size={"sm"} />}
 				</Box>
 			</Flex>
 		</Flex>
